6f0b5e7fb6ff436dce61a537b47e51c4f03498a7
Alexander Serebryakov <alex.serebr@gmail.com>
2013-11-28 14:12:40 +0200

    Added skype, vlc, thunderburd, firefox, vlc

--- pkg-install.sh
+++ pkg-install.sh
@@ -1,16 +1,30 @@
 #!/bin/sh
 
 PKG_MANAGER=apt-get
+ADD_REPO=add-apt-repository
 FLAGS=-y
 INSTALL=install
 UPDATE=install
 UPGRADE=upgrade
 
-echo System update
+echo Adding Repositories
+$ADD_REPO "deb http://archive.canonical.com/ $(lsb_release -sc) partner"
+
+echo System Update
 $PKG_MANAGER $FLAGS $UPDATE
 $PKG_MANAGER $FLAGS $UPGRADE
 
-echo Software installation
+echo Software Installation
 $PKG_MANAGER $FLAGS $INSTALL git
 $PKG_MANAGER $FLAGS $INSTALL ctags
 $PKG_MANAGER $FLAGS $INSTALL vim
+
+$PKG_MANAGER $FLAGS $INSTALL firefox
+$PKG_MANAGER $FLAGS $INSTALL thunderbird
+
+$PKG_MANAGER $FLAGS $INSTALL backintime-gnome
+
+$PKG_MANAGER $FLAGS $INSTALL vlc
+$PKG_MANAGER $FLAGS $INSTALL skype
+$PKG_MANAGER $FLAGS $INSTALL gimp
+$PKG_MANAGER $FLAGS $INSTALL gimp-ufraw
